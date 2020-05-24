5c494d6eeb4ef8c862ca92dff98f4f0303dd6ee4
gstebelova <gstebelova@seznam.cz>
2020-05-24 14:59:04 +0200

    Vzpracování úkolu

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('funguju!');
+const todo__tasks = document.querySelector(".todo__tasks");
+todo__tasks.innerHTML = " ";
+
+const tasks = [];
+
+
+
+function updateTasks() {
+  for (let i = 0; i < tasks.length; i++) {
+    const taskdiv = document.createElement("div")
+    taskdiv.classList.add("task")
+    taskdiv.innerHTML = tasks[i]
+    todo__tasks.appendChild(taskdiv)
+  }
+}
+
+
+const addTask = () => {
+  const inputValue = document.getElementById("new-task").value
+  if (inputValue !== " ") {
+    tasks.push(inputValue)
+    document.getElementById("new-task").value = " "
+    todo__tasks.innerHTML = " ";
+    updateTasks()
+  }
+
+}
+
+
+
